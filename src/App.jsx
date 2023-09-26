b8bcca4bfde5d8e14326a94679c44e646334c644
noah milliken <noahmilliken@gmail.com>
2023-09-26 15:45:09 -0400

    setup .env and API_KEY create PickCard and setup dummydata

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   createRoutesFromElements,
-  Route, 
-  RouterProvider
-} from 'react-router-dom'
-
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 // components
-import Home from './components/Home'
-import Picks from './components/Picks'
-import League from './components/League'
-import Settings from './components/Settings'
+import Home from "./components/Home";
+import Picks from "./components/Picks";
+import League from "./components/League";
+import Settings from "./components/Settings";
 
 // layouts
-import RootLayout from './layouts/RootLayout'
-import { ProfileLayout } from './layouts/ProfileLayout'
-import NotFound from './components/NotFound'
-
+import RootLayout from "./layouts/RootLayout";
+import { ProfileLayout } from "./layouts/ProfileLayout";
+import NotFound from "./components/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="profile" element={<ProfileLayout />} >
-        <Route path='picks' element={<Picks />}/>
-        <Route path='League' element={<League />}/>
-        <Route path='settings' element={<Settings />}/>
+      <Route path="profile" element={<ProfileLayout />}>
+        <Route path="picks" element={<Picks />} />
+        <Route path="League" element={<League />} />
+        <Route path="settings" element={<Settings />} />
       </Route>
 
-      <Route path='*' element={<NotFound />}/>
-    </Route>
-  )
-)
+      <Route path="*" element={<NotFound />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
-export default App
+export default App;
--- src/components/Picks.jsx
+++ src/components/Picks.jsx
@@ -1,6 +1,28 @@
- const Picks = () => {
-  return <div>Picks</div>;
+import { Box, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import Pick from "./PickCard";
+import { games } from "../data/games";
+
+const Picks = () => {
+  console.log(games);
+  return (
+    <Box>
+      <Heading>Pick the Spread</Heading>
+      <Text>Pick the winners for this week</Text>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        {games.map((game, index) => (
+          <Pick
+            key={index}
+            homeTeam={game.homeTeam}
+            awayTeam={game.awayTeam}
+            spread={game.spread}
+          />
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
 };
 
-
-export default Picks
+export default Picks;
