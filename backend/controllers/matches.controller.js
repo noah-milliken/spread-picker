19a8f9beff60f264f5546823ec919186e070305a
noah milliken <noahmilliken@gmail.com>
2023-11-15 16:27:18 -0500

    Implement calculate spread funciton

--- backend/controllers/matches.controller.js
+++ backend/controllers/matches.controller.js
@@ -2,13 +2,35 @@ const Match = require("../models/matches.model");
 
 module.exports = {
   getAll: async (req, res) => {
-    const weeks = await Match.getWeeks();
-    res.send(weeks);
+    try {
+      const weeks = await Match.getWeeks();
+      res.send(weeks);
+    } catch (error) {
+      console.error("Error in matches.weeks", error.message);
+      throw error;
+    }
   },
 
   byWeek: async (req, res) => {
-    const weekNum = req.params.week;
-    const week = await Match.getWeekByNum(weekNum);
-    res.send(week);
+    try {
+      const weekNum = req.params.week;
+      const week = await Match.getWeekByNum(weekNum);
+      res.send(week);
+    } catch (error) {
+      console.error("Error in matches.weekNum", error.message);
+      throw error;
+    }
+  },
+  calculateSpreadResult: async (req, res) => {
+    try {
+      const weekNum = req.params.week;
+      const week = await Match.calculateSpreadResult(weekNum);
+      res.send(200);
+    } catch (error) {
+      console.error("Error in calculateSpreadResult:", error.message);
+      next(error);
+    }
   },
 };
+
+Match.calculateSpreadResult(10);
---
+++ backend/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const message = err.message || "an unknown error occured";
+  res.status(status).json({
+    success: false,
+    status,
+    message,
+    stack: process.env.NODE_ENV !== "development" ? {} : err.stack,
+  });
+};
+
+module.exports = errorHandler;
