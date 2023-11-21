1c30ef5bdaf81ea9c6c1945b9d335c36c99479ed
noah milliken <noahmilliken@gmail.com>
2023-11-21 14:41:16 -0500

    Standings Table created standings table componenet that accepts the league data from database and displays that data as on a tabbed table component.

--- frontend/src/components/Standings.jsx
+++ frontend/src/components/Standings.jsx
@@ -2,32 +2,36 @@
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import StandingsTable from './StandingsTable'
 const Standings = ({userid}) => {
-  const [data, setData] = useState(undefined)
 
+  const [data, setData] = useState([])
     useEffect(()=> {
       const fetchData = async () => {
         try {
-          const response = await axios.get('')
+          const response = await axios.get(`http://localhost:8080/dashboard/${userid}/getStandings/10`)
           setData(response.data)
         } catch (error) {
           console.error(error)
         }
       }
       fetchData()
-
     }, [userid])
-    console.log(data)
+
   return (
     <Tabs isFitted variant='line'>
-      <TabList>
-        <Tab>Generic gamers</Tab>
-        <Tab>Polite Pirates</Tab>
-        <Tab>Frought Fidgiters</Tab>
-        <Tab>Placid Pick-makers</Tab>
+         <TabList>
+        {data.map((league, index) => (
+          <Tab key={index}>{Object.keys(league)[0]}</Tab>
+        ))}
       </TabList>
       <TabPanels>
-
+        {data.map((league, index) => (
+          <TabPanel key={index}>
+            <StandingsTable  league={league}/>
+          </TabPanel>
+        ))}
+          
       </TabPanels>
     </Tabs>
   )
