b5f8ce85f13c90bd7ad9f230143cc19385b0ff43
noah milliken <noahmilliken@gmail.com>
2023-11-28 13:02:01 -0500

    remove nfl stats package

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const nfl = require("nfl-stats");
 
 const dotenv = require("dotenv");
 dotenv.config();
