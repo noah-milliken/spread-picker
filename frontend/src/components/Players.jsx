31c904f1e462a126e98f531e46dff3e15f083af3
noah milliken <noahmilliken@gmail.com>
2023-12-19 13:34:05 -0500

    Setup protected routes based on Auth Added protected route on front end. added landing page to default path at routes root.

--- frontend/src/components/Players.jsx
+++ frontend/src/components/Players.jsx
@@ -1,67 +1,67 @@
-import { Button, Flex, Text } from '@chakra-ui/react';
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { Button, Flex, Text } from '@chakra-ui/react';
+// import axios from 'axios'
+// import { useEffect, useState } from 'react';
 
-const Home = () => {
-const [users, setUsers] = useState([])
 
-useEffect(() => {
-  axios.get('http://localhost:8080/users/')
-  .then(res => {
-    console.log(res.data)
-    setUsers(res.data)
-  }).catch(err => {
-    console.log(err)
-  })
+// const Home = () => {
+// const [users, setUsers] = useState([])
 
-},[])
+// useEffect(() => {
+//   axios.get('http://localhost:8080/users/')
+//   .then(res => {
+//     console.log(res.data)
+//     setUsers(res.data)
+//   }).catch(err => {
+//     console.log(err)
+//   })
 
-  return (
-    <Flex
-    minH={'100%'}
-    minWidth={'100%'}
-   >
-      <Flex 
-      bg={'purple.300'}
-      display={{ base: "none", sm: "block" }}
-      border={'solid'}
-      minH={'100%'}
-      minW={'50%'}
-      maxW={'50%'}
-      didplay={['none', 'flex']}
-      >
-      
-      </Flex>
+// },[])
+
+//   return (
+//     <Flex
+//     minH={'100%'}
+//     minWidth={'100%'}
+//    >
+//       <Flex 
+//       bg={'purple.800'}
+//       display={{ base: "none", sm: "block" }}
+//       border={'solid'}
+//       minH={'80vh'}
+//       minW={'100%'}
+//       maxW={'100%'}
+//       didplay={['none', 'flex']}
+//       >
+//       Welcome to spread-picker
+//       </Flex>
     
-    <Flex
-    border={'1px tomato solid'}
-    pl={6}
-    direction={'column'}
-    height={'100%'}
-    minWidth={'50%'}
-    gap={3}
-    >
-      {users.map((user, index) => (
-        <Flex  
-        key={index}
-        align={'flex-start'}
-        justifyContent={'space-between'}
-        alignItems={'center'}
-        >
-            <Text>{user.first_name}   {user.last_name}</Text>
-            <Button variant={'outline'} 
-            onClick={console.log(`${user.user_id}`)}
-            >
-              <NavLink to={`profile/${user.user_id}`}>Login</NavLink>
-            </Button>
-        </Flex>
-      ))}
+//     {/* <Flex
+//     border={'1px tomato solid'}
+//     pl={6}
+//     direction={'column'}
+//     height={'100%'}
+//     minWidth={'50%'}
+//     gap={3}
+//     >
+//       {users.map((user, index) => (
+//         <Flex  
+//         key={index}
+//         align={'flex-start'}
+//         justifyContent={'space-between'}
+//         alignItems={'center'}
+//         >
+//             <Text>{user.first_name}   {user.last_name}</Text>
+//             <Button variant={'outline'} 
+//             onClick={console.log(`${user.user_id}`)}
+//             >
+//               <NavLink to={`profile/${user.user_id}`}>Login</NavLink>
+//             </Button>
+//         </Flex>
+//       ))}
 
-    </Flex>
-    </Flex>
+//     </Flex> */}
+//     </Flex>
   
-    )
-};
+//     )
+// };
 
-export default Home;
+// export default Home;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,28 +1,52 @@
-import { useEffect, useState } from "react";
-import {Flex} from '@chakra-ui/react'
-import axios from 'axios'
-import { useParams } from "react-router-dom";
-import Standings from "./Standings";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import PageLayout from "../utils/PageLayout";
+import { CodeSnippet } from "../utils/CodeSnippet";
 const Profile = () => {
-  const {userid} = useParams()
-  const [profileData, setProfileData] = useState([])
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8080/users/${userid}/profile/`)
-        setProfileData(response.data)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchData()
-  },[])
+  const { user } = useAuth0();
 
-  
-  return (<Flex>
-      <Standings userid={userid} />
-  </Flex>)
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <PageLayout>
+    <div className="content-layout">
+      <h1 id="page-title" className="content__title">
+        Profile Page
+      </h1>
+      <div className="content__body">
+        <p id="page-description">
+          <span>
+            You can use the <strong>ID Token</strong> to get the profile
+            information of an authenticated user.
+          </span>
+          <span>
+            <strong>Only authenticated users can access this page.</strong>
+          </span>
+        </p>
+        <div className="profile-grid">
+          <div className="profile__header">
+            <img
+              src={user.picture}
+              alt="Profile"
+              className="profile__avatar"
+            />
+            <div className="profile__headline">
+              <h2 className="profile__title">{user.name}</h2>
+              <span className="profile__description">{user.email}</span>
+            </div>
+          </div>
+          <div className="profile__details">
+            <CodeSnippet
+              title="Decoded ID Token"
+              code={JSON.stringify(user, null, 2)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </PageLayout>
+  );
 };
 
 export default Profile
